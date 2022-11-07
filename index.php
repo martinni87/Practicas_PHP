59cb6bdac130bb4bc71635e9deb382b517c698ee
Martin Cordoba <mancorge@gmail.com>
2022-11-07 19:55:58 +0100

    Added practica1.2 to README. Update of index.php for practica1.2. Practica 1.2 done and working

--- index.php
+++ index.php
@@ -1,19 +1,31 @@
+<!-- Añadimos a la cabeza del documento este fragmento de código php que procesará los datos del formulario -->
 <?php
     /*echo "<pre>";
     print_r($_POST);
     echo "</pre>";*/
 
-    $rows = 0;
-    $columns = 0;
+    $rows = 2; //En la primera carga forzamos tabla 2x2
+    $columns = 2; //En la primera carga forzamos tabla 2x2
 
     if (isset($_POST["rows"])){ //Comprobamos que existe
-        $rows = $_POST["rows"];
+        if ($_POST["rows"] < 1){ //Caso valor menor a 1, asignamos valor = 2
+            $rows = 2;
+        }
+        else{
+            $rows = $_POST["rows"]; //Caso valor >= 1, asignamos valor desde array $_POST
+        }
     }
-    if (isset($_POST["columns"])){ //Comprobamos que existe
-        $columns = $_POST["columns"];
+    if (isset($_POST["columns"])){//Comprobamos que existe
+        if ($_POST["columns"] < 1){ //Caso valor menor a 1, asignamos valor = 2
+            $columns = 2;
+        }
+        else{
+            $columns = $_POST["columns"]; //Caso valor >= 1, asignamos valor desde array $_POST
+        }
     }
 ?>
 
+<!-- Cabecera del documento HTML -->
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -23,9 +35,6 @@
     <meta name="keywords" content="HTML, PHP, DAM, Ejercicios Desarrollo de Interfaces">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!--<link rel="icon" type="image/x-icon" href="./img/logo.png">
-    <link rel="stylesheet" href="./css/estilos.css">-->
-
     <title>Práctica 1 Desarrollo de Interfaces</title>
 </head>
 <body>
@@ -34,17 +43,17 @@
     <!-- Para que un usuario introduzca la cantidad de columnas y filas manualmente, debemos crear un formulario -->
     <form method = "post" action = "index.php"> <!-- method get o post, ver www.w3schools.com/tags/att_form_method.asp. Action es el documento a donde mandamos la info para procesarla, en este caso este mismo documento php-->
                                                 <!-- añadimos a la cabecera al principio de este documento lo que va a procesar el formulario -->
-        <label for="rows">Filas</label><input type="number" name="rows" id="rows" value="<?php echo $rows ?>">
-        <label for="columns">Columnas</label><input type="number" name="columns" id="columns" value="<?php echo $columns ?>">
-        <input type="submit" value="Enviar" name="enviar" id="enviar">
+        <fieldset>
+            <legend>Datos para la tabla</legend>
+            <label for="rows">Filas</label><input type="number" name="rows" id="rows" value="<?php echo $rows ?>">
+            <label for="columns">Columnas</label><input type="number" name="columns" id="columns" value="<?php echo $columns ?>">
+            <input type="submit" value="Enviar" name="enviar" id="enviar">
+        </fieldset>
     </form>
-    <br>
-    <!--
-    No forzar nunca saltos de linea en html, es mala práctica. Hacerlo luego con CSS
-    Ver más abajo, el comentario sobre la estructura de tabla que queremos crear
-    Creamos una matriz (tabla) indicando el número de columnas y filas -->
+    <br> <!-- No es buena práctica usar saltos de línea en html, mejor hacerlo mediante CSS. Se deja así porque esta práctica se enfoca solo en php -->
+    <!-- Creamos una matriz (tabla) indicando el número de columnas y filas -->
     
-    <table border="1">
+    <table border="1"> <!-- Tampoco es buena práctica poner border aquí, es mejor en CSS -->
         <!-- Cabecera de la tabla, una única fila (1 tr). tantas th como columnas haya -->
         <thead>
             <tr>
