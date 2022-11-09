ae8b8afb06f23e202e3478ad54ae35a1ddf7d1a7
Martin Antonio <mancorge@gmail.com>
2022-11-10 00:02:06 +0100

    Almost everything working. Pending to check overall functioning and how to not erase what user types the first time.

--- index.php
+++ index.php
@@ -4,15 +4,49 @@
     $username = 'root';
     $password = '';
 
+    //Variables introducidas por el usuario. Lo heredamos del método POST
+    $filtros = $_POST;
+
     //Hacemos un try-catch para capturar errores en caso de que la conexión falle de alguna manera.
     try{
         //Creando conexión a Base de datos
         $con = new PDO($hostdbname,$username,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
         $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+        //Array que recoge parámetros sólo si el usuario ha utilizado un determinado filtro.
+        $arr_where = array();
+        $parametros = array();
+
+        //Creamos una variable para realizar la query. Esta variable es un String va cambiando según si el usuario usa un filtro o no
+        $sql = 'SELECT dni, nombre, apellido_1, apellido_2, localidad, fecha_nacimiento FROM alumno WHERE true';
+
+        //Condiciones de filtros. Se añaden si el usuario indica un valor en un filtro y son acumulativos. Definen $sql y $parametros
+        if (isset($filtros["dni"]) && !empty($filtros["dni"])){
+            $sql .= " AND dni = :dni";
+            $parametros[":dni"] = $filtros["dni"];
+        }
+        if (isset($filtros["nombre"]) && !empty($filtros["nombre"])){
+            $sql .= " AND nombre LIKE :nombre";
+            $parametros[":nombre"] = "%".$filtros["nombre"]."%";
+        }
+        if (isset($filtros["localidad"]) && !empty($filtros["localidad"])){
+            $sql .= " AND localidad LIKE :localidad";
+            $parametros[":localidad"] = "%".$filtros["localidad"]."%";
+        }
+        if (isset($filtros["fecha_nacimiento"]) && !empty($filtros["fecha_nacimiento"])){
+            $sql .= " AND fecha_nacimiento > :fecha_nacimiento";
+            $parametros[":fecha_nacimiento"] = $filtros["fecha_nacimiento"];
+        }
+
+        echo $sql.'<br/>';
+        echo empty($filtros);
+
         //Definimos el stmt con prepare. El orden de los valores que devolverá el SELECT lo definimos aquí.
-        $stmt = $con -> prepare('SELECT dni,nombre,apellido_1,apellido_2,localidad,fecha_nacimiento FROM alumno');
-        $stmt->execute();
+        $stmt = $con -> prepare($sql);
+        
+        // Con execute parametrizamos la query y asignamos los valores a las variables de usuario
+        $stmt->execute($parametros);
+
     //No cierro la llave del try aquí, va más abajo con el catch para darle continuidad y que no de error de catch sin try.
 ?>
 <!-- Cabecera del documento HTML -->
@@ -44,7 +78,13 @@
 
     <form action="index.php" method="post">
         <fieldset>
-            
+            <legend>Filtros de búsqueda</legend>
+            <label for="dni">DNI:</label><input type="text" name="dni" id="dni" placeholder="01234567A" title="Escribe un DNI">
+            <label for="nombre">Nombre:</label><input type="text" name="nombre" id="nombre" placeholder="Pep" title="Escribe un nombre">
+            <label for="localidad">Localidad:</label><input type="text" name="localidad" id="licalidad" placeholder="Elche" title="Escribe una localidad">
+            <label for="fecha_nacimiento">F. Nacimiento:</label><input type="date" name="fecha_nacimiento" id="fecha_nacimiento" placeholder="20/05/1991" title="Escribe una fecha en formato DD/MM/AAAA">
+            <input type="submit" value="Enviar" name="enviar" id="enviar">
+            <input type="reset" value="Limpiar" name="limpiar" id="limpiar">
         </fieldset>
     </form>
 
@@ -63,7 +103,7 @@
         </thead>
 
     <?php
-            // Abrimos cuerpo de la tabla
+            // Estructura de la tabla (solo cuerpo)
             echo '<tbody>';
             while($datos = $stmt -> fetch(PDO::FETCH_ASSOC)){
                 echo '<tr>'; // Abrimos fila
