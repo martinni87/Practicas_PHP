4d6cf1e3c0e30d572875bb8ef3d3cbe27a94d920
Martin Antonio <mancorge@gmail.com>
2022-11-10 22:50:29 +0100

    Some changes html to php

--- README.md
+++ README.md
@@ -41,11 +41,11 @@ En este ejercicio vamos a practicar las conexiones a BD mediante la clase PDO. P
 una conexión a la BD universidad y obtener el listado de alumnos de la BD mostrándolo en una tabla,
 tal como muestra la imagen.
 
-![Ejemplo1.3](./img/pic_ejemplo_1_3.JPG "Ejemplo 1.3.")
+![Ejemplo1.3](./img/pic_ejemplo_1_3.JPG 'Ejemplo 1.3.')
 
 Lo vamos a hacer en el archivo index de la rama descartando todo el código que teníamos. Como siempre, el documento será un HTML5 bien formado aunque con extensión PHP para embeber nuestros scripts.
 
-De las distintas formas de hacerlo que tenéis en el manual adjunto en moodle, vamos a realizarlo con el método "prepare" ya que aporta robustez, y en un cuanto a la forma de rellenar la tabla se deja a criterio del alumno.
+De las distintas formas de hacerlo que tenéis en el manual adjunto en moodle, vamos a realizarlo con el método 'prepare' ya que aporta robustez, y en un cuanto a la forma de rellenar la tabla se deja a criterio del alumno.
 
 Para no tener problemas con la codificación alfanumérica de la BD tendréis que forzar que la conexión a la BD se haga con UTF-8
 
@@ -56,30 +56,30 @@ En este ejercicio vamos a crear un formulario que permita el envío de datos par
 
 Lo vamos a hacer en el archivo index completando lo realizado en la práctica 1.3.
 
-![Ejemplo1.4](./img/pic_ejemplo_1_4.JPG "Ejemplo 1.4.")
+![Ejemplo1.4](./img/pic_ejemplo_1_4.JPG 'Ejemplo 1.4.')
 
-De las distintas formas de hacerlo que tenéis en el manual adjunto en moodle, vamos a realizarlo con el método "prepare" ya que aporta mayor robustez y parametrizando la query con un array en el execute.
+De las distintas formas de hacerlo que tenéis en el manual adjunto en moodle, vamos a realizarlo con el método 'prepare' ya que aporta mayor robustez y parametrizando la query con un array en el execute.
 
-Las búsquedas se realizarán aplicando AND en el WHERE, de tal manera que si busco por nombre "Juan" y localidad "Arneva" tendrá que cumplir ambos criterios.
+Las búsquedas se realizarán aplicando AND en el WHERE, de tal manera que si busco por nombre 'Juan' y localidad 'Arneva' tendrá que cumplir ambos criterios.
 
 ## Descripción 1.5
 En este ejercicio vamos a crear un paginador que permita navegar por las distintas páginas y seleccionar el número de registros que queremos visualizar teniendo en cuenta los filtros aplicados.
 
 Lo vamos a hacer en el archivo index completando lo realizado en la práctica 1.4
 
-![Ejemplo1.5](./img/pic_ejemplo_1_5.JPG "Ejemplo 1.5.")
+![Ejemplo1.5](./img/pic_ejemplo_1_5.JPG 'Ejemplo 1.5.')
 
 ### Acciones del paginador
 
-* <<. Nos carga la primera página teniendo en cuenta el filtro aplicado y el número de registros por página. Será un botón submit con name = "primera".
-* <. Cargará la página anterior a la que estamos, siempre que se pueda. Será un botón submit con name = "anterior".
-* >. Cargará la página siguiente a la que estemos, siempre que se pueda. Será un botón submit con name = "siguiente".
+* <<. Nos carga la primera página teniendo en cuenta el filtro aplicado y el número de registros por página. Será un botón submit con name = 'primera'.
+* <. Cargará la página anterior a la que estamos, siempre que se pueda. Será un botón submit con name = 'anterior'.
+* >. Cargará la página siguiente a la que estemos, siempre que se pueda. Será un botón submit con name = 'siguiente'.
 * >>. Nos carga la última página teniendo en cuenta el filtro aplicado y el número de registros por página. Será un select con los valores de 10, 15, 20 y todos estos datos también serán su value.
-* Mostrar. Será un botón de tipo submit con name = "mostrar" que, además de enviar el formulario, pondrá la página a 1.
+* Mostrar. Será un botón de tipo submit con name = 'mostrar' que, además de enviar el formulario, pondrá la página a 1.
 
 ### Aclaraciones y recomendaciones
 * El formulario contendrá todo, desde los filtros al paginador, con objeto de enviar y poder recoger todos los valores necesarios en la petición.
 * Se definen varios submit para enviar el formulario en cada acción definida, en estos casos es muy importante el name para saber qué acción es la que se pulsó.
 * Para el cálculo de los registros por página se puede hacer uso de la cláusula LIMIT que facilita la obtención de los registros que necesitamos. Ejem. limit 0, 10 traerá desde el primero y un total de 10. El 10 será el valor de la opción seleccionada en el SELECT y el registro por el que vamos se calcula multiplicando la página-1 por el número de registros por página, aplicando el entero por encima. Ya que si hay 72 registros serán 8 páginas.
 * Para obtener el total de registros, teniendo en cuenta los filtros aplicados, no el LIMIT, haremos una primera consulta con COUNT *, almacenamos el valor y luego haremos la consulta para obtener los datos. Se debe utilizar la misma conexión, es decir, reutilizamos el objeto PDO iniciado al principio.+
-* Para que quede seleccionado el SELECT con la option elegida en la recarga se puede hacer en cada option, que si el valor del número de registros recogido por el POST es igual al valor del option añada la proepiedad "selected".
+* Para que quede seleccionado el SELECT con la option elegida en la recarga se puede hacer en cada option, que si el valor del número de registros recogido por el POST es igual al valor del option añada la proepiedad 'selected'.
--- index.php
+++ index.php
@@ -4,68 +4,82 @@
     $username = 'root';
     $password = '';
 
-    //Variables introducidas por el usuario. Lo heredamos del método POST
-    $filtros = $_POST;
-
     //Hacemos un try-catch para capturar errores en caso de que la conexión falle de alguna manera.
     try{
+        //Para debug
+        // foreach ($_POST as $key => $value) {
+        //     echo $key . ' = ' . $value . '<br/>';
+        // }
+        // echo $sql;
+
+        // //Variables introducidas por el usuario. Lo heredamos del método POST
+         $filtros = $_POST;   
         //Creando conexión a Base de datos
-        $con = new PDO($hostdbname,$username,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")); 
+        $con = new PDO($hostdbname,$username,$password,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8')); 
         $con -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // Vaciado de filtro cuando se presiona uno de los botones del formulario y se trata del reset.
+        if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+            if(isset($_POST['reset'])){
+                $_POST = [];
+                //echo 'Reset button pressed<br/>'; // Para debug, con esto vemos un mensaje cuando se presiona el botón reset
+            }
+        }
 
         //Array que recoge parámetros sólo si el usuario ha utilizado un determinado filtro.
         $parametros = array();
 
-        //Creamos una variable para realizar la query. Esta variable es un String va cambiando según si el usuario usa un filtro o no
-        $sql = 'SELECT dni, nombre, apellido_1, apellido_2, localidad, fecha_nacimiento FROM alumno WHERE true';
-
-        // Vaciado de filtro cuando se presiona uno de los botones del formulario y se trata del reset.
-        if ($_SERVER["REQUEST_METHOD"] === "POST"){
-            if(isset($_POST["reset"])){
-                $_POST = [];
-                echo 'Reset button pressed<br/>'; // Para debug, con esto vemos un mensaje cuando se presiona el botón reset
-            }
-        }
+        //Creamos una variable que recibirá la parte de filtros a aplicar en la query
+        $sql_filtros = '';
 
         //Condiciones de filtros. Se añaden si el usuario indica un valor en un filtro y son acumulativos. Definen $sql y $parametros
+        if (isset($_POST['dni']) && !empty($_POST['dni'])){
+            $sql_filtros .= ' AND dni LIKE :dni';
+            $parametros[':dni'] = '%'.$_POST['dni'].'%'; //No hago que sea resultado exacto porque un usuario puede querer ver todos los dnis que tienen por ejemplo 111.
+        }
+        if (isset($_POST['nombre']) && !empty($_POST['nombre'])){
+            $sql_filtros .= ' AND nombre LIKE :nombre';
+            $parametros[':nombre'] = '%'.$_POST['nombre'].'%';
+        }
+        if (isset($_POST['localidad']) && !empty($_POST['localidad'])){
+            $sql_filtros .= ' AND localidad LIKE :localidad';
+            $parametros[':localidad'] = '%'.$_POST['localidad'].'%';
+        }
+        if (isset($_POST['fecha_nacimiento']) && !empty($_POST['fecha_nacimiento'])){
+            $sql_filtros .= ' AND fecha_nacimiento > :fecha_nacimiento';
+            $parametros[':fecha_nacimiento'] = $_POST['fecha_nacimiento'];
+        }
 
-        if (isset($filtros["dni"]) && !empty($filtros["dni"])){
-            $sql .= " AND dni LIKE :dni";
-            $parametros[":dni"] = "%".$filtros["dni"]."%"; //No hago que sea resultado exacto porque un usuario puede querer ver todos los dnis que tienen por ejemplo 111.
+        //Terminamos de construir la query. Si se presiona submit mostramos datos, si no, no se muestra nada.
+        if(isset($_POST['submit'])){
+            $sql = 'SELECT dni, nombre, apellido_1, apellido_2, localidad, fecha_nacimiento FROM alumno WHERE true' . $sql_filtros;
+            //Definimos el stmt con prepare y parametrizamos la query
+            $stmt = $con -> prepare($sql);
+            $stmt->execute($parametros);
         }
-        if (isset($filtros["nombre"]) && !empty($filtros["nombre"])){
-            $sql .= " AND nombre LIKE :nombre";
-            $parametros[":nombre"] = "%".$filtros["nombre"]."%";
-        }
-        if (isset($filtros["localidad"]) && !empty($filtros["localidad"])){
-            $sql .= " AND localidad LIKE :localidad";
-            $parametros[":localidad"] = "%".$filtros["localidad"]."%";
-        }
-        if (isset($filtros["fecha_nacimiento"]) && !empty($filtros["fecha_nacimiento"])){
-            $sql .= " AND fecha_nacimiento > :fecha_nacimiento";
-            $parametros[":fecha_nacimiento"] = $filtros["fecha_nacimiento"];
-        }
-
-        echo $sql.'<br/>';
-
-        //Definimos el stmt con prepare. El orden de los valores que devolverá el SELECT lo definimos aquí.
-        $stmt = $con -> prepare($sql);
-        
-        // Con execute parametrizamos la query y asignamos los valores a las variables de usuario
-        $stmt->execute($parametros);
+        //$sql .= $sql_filtros;
+        // //Hacemos un segundo SELECT para saber la cantidad total de registros con los filtros aplicados
+        // $sql = 'SELECT COUNT(*) FROM alumno WHERE true';
+        // $sql .= $sql_filtros;
+        // if ($_POST['registros'] != 'Todos'){
+        //     $sql .= ' LIMIT ' . $regini . ',' . $regfin;
+        // }
+        // //Definimos un nuevo stmt con prepare y parametrizamos la query
+        // $stmt = $con -> prepare($sql);
+        // $stmt->execute($parametros);
 
     //No cierro la llave del try aquí, va más abajo con el catch para darle continuidad y que no de error de catch sin try.
 ?>
 <!-- Cabecera del documento HTML -->
 <!DOCTYPE html>
-<html lang="es">
+<html lang='es'>
 <head>
-    <meta charset="UTF-8">
-    <meta name="author" content="Martín Antonio Córdoba Getar">
-    <meta name="description" content="Práctica de Desarrollo de Interfaces 2ºDAM EFA El Campico">
-    <meta name="keywords" content="HTML, PHP, DAM, Ejercicios Desarrollo de Interfaces">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset='UTF-8'>
+    <meta name='author' content='Martín Antonio Córdoba Getar'>
+    <meta name='description' content='Práctica de Desarrollo de Interfaces 2ºDAM EFA El Campico'>
+    <meta name='keywords' content='HTML, PHP, DAM, Ejercicios Desarrollo de Interfaces'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Práctica 1 Desarrollo de Interfaces</title>
     <!-- Estilos para la tabla, esto es mejor crearlo en archivos CSS aparte y lo hago solo por este paquete de prácticas -->
     <style>
@@ -81,53 +95,70 @@
     <!-- Títulos -->
     <h1>Práctica 1 Desarrollo de Interfaces</h1>
     <h2>Alumno: Martín Antonio Córdoba Getar</h2>
-    <h3>Práctica 1.4 DI</h3>
+    <h3>Práctica 1.5 DI</h3>
 
-    <form action="index.php" method="post">
+    <form action='index.php' method='post'>
         <fieldset>
             <legend>Filtros de búsqueda</legend>
-            <label for="dni">DNI:</label><input type="text" name="dni" id="dni" placeholder="01234567A" title="Escribe un DNI" value="<?php echo $_POST["dni"] ?>">
-            <label for="nombre">Nombre:</label><input type="text" name="nombre" id="nombre" placeholder="Pep" title="Escribe un nombre" value="<?php echo $_POST["nombre"] ?>">
-            <label for="localidad">Localidad:</label><input type="text" name="localidad" id="licalidad" placeholder="Elche" title="Escribe una localidad" value="<?php echo $_POST["localidad"] ?>">
-            <label for="fecha_nacimiento">F. Nacimiento:</label><input type="date" name="fecha_nacimiento" id="fecha_nacimiento" placeholder="20/05/1991" title="Escribe una fecha en formato DD/MM/AAAA" value="<?php echo $_POST["fecha_nacimiento"] ?>">
-            <button type="submit" name="submit" id="submit">Enviar</button>
-            <button type="submit" name="reset" id="reset">Limpiar</button> <!-- Si aplico type submit en lugar de reset puedo variar el valor según se presiona enviar o limpiar de forma que con isset($_POST["reset"] pueda aplicar $_POST = [] y vaciar los filtros -->
+            <label for='dni'>DNI:</label><input type='text' name='dni' id='dni' placeholder='01234567A' title='Escribe un DNI' value='<?php echo $_POST['dni'] ?>'>
+            <label for='nombre'>Nombre:</label><input type='text' name='nombre' id='nombre' placeholder='Pep' title='Escribe un nombre' value='<?php echo $_POST['nombre'] ?>'>
+            <label for='localidad'>Localidad:</label><input type='text' name='localidad' id='licalidad' placeholder='Elche' title='Escribe una localidad' value='<?php echo $_POST['localidad'] ?>'>
+            <label for='fecha_nacimiento'>F. Nacimiento:</label><input type='date' name='fecha_nacimiento' id='fecha_nacimiento' placeholder='20/05/1991' title='Escribe una fecha en formato DD/MM/AAAA' value='<?php echo $_POST['fecha_nacimiento'] ?>'>
+            <button type='submit' name='submit' id='submit'>Enviar</button>
+            <button type='submit' name='reset' id='reset'>Limpiar</button> <!-- Si aplico type submit en lugar de reset puedo variar el valor según se presiona enviar o limpiar de forma que con isset($_POST['reset'] pueda aplicar $_POST = [] y vaciar los filtros -->
         </fieldset>
-    </form>
-
-    <p>Listado de alumnos</p>
-    <!-- Estructura de la tabla (solo cabecera) -->
-    <table>
-        <thead>
-            <tr>
-                <th>DNI</th>
-                <th>Nombre</th>
-                <th>Apellido 1</th>
-                <th>Apellido 2</th>
-                <th>Localidad</th>
-                <th>F. Nacimiento</th>
-            </tr>
-        </thead>
-
-    <?php
-            // Estructura de la tabla (solo cuerpo)
-            echo '<tbody>';
-            while($datos = $stmt -> fetch(PDO::FETCH_ASSOC)){
-                echo '<tr>'; // Abrimos fila
-                foreach ($datos as $key => $value) {
-                    echo '<td>' . $value . '</td>'; // En cada iteración pintamos un valor del array, el orden lo da el SELECT del $stmt
+    <?php 
+    if (isset($_POST['submit'])){
+        echo "
+        <p>Listado de alumnos</p>
+        <!-- Estructura de la tabla (solo cabecera) -->
+        <table>
+            <thead>
+                <tr>
+                    <th>DNI</th>
+                    <th>Nombre</th>
+                    <th>Apellido 1</th>
+                    <th>Apellido 2</th>
+                    <th>Localidad</th>
+                    <th>F. Nacimiento</th>
+                </tr>
+            </thead>
+                // Estructura de la tabla (solo cuerpo)
+                echo '<tbody>';
+                while($datos = $stmt -> fetch(PDO::FETCH_ASSOC)){
+                    echo '<tr>'; // Abrimos fila
+                    foreach ($datos as $key => $value) {
+                        echo '<td>' . $value . '</td>'; // En cada iteración pintamos un valor del array, el orden lo da el SELECT del $stmt
+                    }
+                    echo '</tr>'; //Cerramos fila
                 }
-                echo '</tr>'; //Cerramos fila
-            }
-            // Cerramos cuerpo de la tabla
-            echo '</tbody>';
-
+                // Cerramos cuerpo de la tabla
+                echo '</tbody>';
+        </table>
+        <br/>
+            <button type='submit' name='primera' id='primera'><<</button>
+            <button type='submit' name='anterior' id='anterior'><</button>
+            <input type='text' name='pagina' id='pagina' value='<?php echo $pagina ?>' disabled='false'>
+            <button type='submit' name='siguiente' id='siguiente'>></button>
+            <button type='submit' name='ultima' id='ultima'>>></button>
+            <label for='registros'>Registros por página:<select name='registros' id='registros' value='<?php echo $registros ?>'>
+                <option value='<?php echo $registros ?> ' selected hidden><?php echo $registros ?></option>
+                <option value='10'>10</option>
+                <option value='15'>15</option>
+                <option value='20'>20</option>
+                <option value='Todos'>Todos</option>
+            </select>
+            <button type='submit' name='mostrar' id='mostrar'>Mostrar</button>
+            '<p>Núm. Registros: '. $numreg . '. Página ' . $paginaActual . '/' . $paginasTotales . '</p>'
+        ";
+    }
+    ?>
+    </form>
+    <?php
         //Si try falla, se lanza excepción PDO
         }catch(PDOException $e){
             echo 'Error en la conexión a la BD: ' . $e -> getMessage();
         }
     ?>
-    </table>
-
 </body>
 </html>
